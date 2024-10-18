0ffe250342b02d6a241e6fff41ecc16a361000e5
한조pc <gps95wwkhhev@gmail.com>
2024-10-18 13:23:36 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/hanjo/portfolio/domain/entity/Achievement.kt
+++ src/main/kotlin/com/hanjo/portfolio/domain/entity/Achievement.kt
@@ -1,9 +1,16 @@
 package com.hanjo.portfolio.domain.entity
 
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
-class Achievement : BaseEntity() {
+class Achievement(
+        title: String,
+        description: String,
+        achievedDate: LocalDate?,
+        host: String,
+        isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,5 +19,15 @@ class Achievement : BaseEntity() {
     // val achievement: Achievement
     // achievement.achievementId
     // achievement.id
+
+    var title: String = title
+
+    var description: String = description
+
+    var achievedDate: LocalDate? = achievedDate
+
+    var host: String = host
+
+    var isActive: Boolean = isActive
 
 }
--- src/main/kotlin/com/hanjo/portfolio/domain/entity/Introduction.kt
+++ src/main/kotlin/com/hanjo/portfolio/domain/entity/Introduction.kt
@@ -3,11 +3,18 @@ package com.hanjo.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Introduction : BaseEntity() {
+class Introduction(
+        content: String,
+        isActive: String
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "introduction_id")
     var id: Long? = null
+
+    var content: String = content
+
+    var isActive: String = isActive
 
 }
