75eaf853867f3eca165614314f4c4d8cc749f214
한조pc <gps95wwkhhev@gmail.com>
2024-10-20 18:16:58 +0900

    리포지토리 개발

--- src/main/kotlin/com/hanjo/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/hanjo/portfolio/domain/repository/ExperienceRepository.kt
@@ -3,4 +3,8 @@ package com.hanjo.portfolio.domain.repository
 import com.hanjo.portfolio.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ExperienceRepository : JpaRepository<Experience, Long>
+interface ExperienceRepository : JpaRepository<Experience, Long> {
+
+    fun findAllByIsActive(isActive: Boolean): List<Experience>
+
+}
--- src/main/kotlin/com/hanjo/portfolio/domain/repository/HttpInterfaceRepository.kt
+++ src/main/kotlin/com/hanjo/portfolio/domain/repository/HttpInterfaceRepository.kt
@@ -2,5 +2,9 @@ package com.hanjo.portfolio.domain.repository
 
 import com.hanjo.portfolio.domain.entity.HttpInterface
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDateTime
 
-interface HttpInterfaceRepository : JpaRepository<HttpInterface, Long>
+interface HttpInterfaceRepository : JpaRepository<HttpInterface, Long> {
+
+    fun countAllByCreatedDateTimeBetween(start: LocalDateTime, end: LocalDateTime)
+}
