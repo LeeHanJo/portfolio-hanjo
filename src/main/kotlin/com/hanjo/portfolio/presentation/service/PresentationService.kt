6f3edb77ebbae3d859a6e5c73e3aaf8e63769485
한조pc <gps95wwkhhev@gmail.com>
2024-10-24 22:55:37 +0900

    서비스 개발

--- src/main/kotlin/com/hanjo/portfolio/presentation/service/PresentationService.kt
+++ src/main/kotlin/com/hanjo/portfolio/presentation/service/PresentationService.kt
@@ -1,6 +1,51 @@
 package com.hanjo.portfolio.presentation.service
 
+import com.hanjo.portfolio.domain.entity.Project
+import com.hanjo.portfolio.presentation.dto.IntroductionDTO
+import com.hanjo.portfolio.presentation.dto.LinkDTO
+import com.hanjo.portfolio.presentation.dto.ProjectDTO
+import com.hanjo.portfolio.presentation.dto.ResumeDTO
+import com.hanjo.portfolio.presentation.repository.PresentationRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class PresentationService
+class PresentationService(
+    private val presentationRepository: PresentationRepository
+) {
+
+    @Transactional(readOnly = true)
+    fun getIntroductions(): List<IntroductionDTO> {
+        val introductions = presentationRepository.getActiveIntroduction()
+
+        return introductions.map { IntroductionDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getLinks(): List<LinkDTO> {
+        val links = presentationRepository.getActiveLink()
+
+        return links.map { LinkDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getResume() : ResumeDTO {
+        val experiences = presentationRepository.getActiveExperience()
+        val achievements = presentationRepository.getActiveAchievement()
+        val skills = presentationRepository.getActiveSkill()
+
+        return ResumeDTO(
+            experiences = experiences,
+            achievements = achievements,
+            skills = skills
+        )
+    }
+
+    @Transactional(readOnly = true)
+    fun getProject(): List<ProjectDTO> {
+        val projects = presentationRepository.getActiveProject()
+
+        return projects.map { ProjectDTO(it) }
+    }
+
+}
